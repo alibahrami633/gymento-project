825e64c9da573e1ec206bede42980e051845f53c
Aliagho <ali.bahrami633@yahoo.com>
2020-07-30 13:19:49 +1000

    added usersList dummy data

--- gymento/src/user/pages/Users.js
+++ gymento/src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>Users Page</h2>;
+  const USERS = [
+    {
+      id: "u01",
+      name: "Ali Bahrami",
+      image: "https://randomuser.me/api/portraits/lego/2.jpg",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
