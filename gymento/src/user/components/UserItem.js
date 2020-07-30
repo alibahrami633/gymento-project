84df72df11f4b03d23433e66b20a688b4eefb029
Aliagho <ali.bahrami633@yahoo.com>
2020-07-30 13:46:29 +1000

    added user cards

--- gymento/src/user/components/UserItem.js
+++ gymento/src/user/components/UserItem.js
@@ -1,20 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./UserItem.css";
+import Avatar from "../../shared/components/UIElements/Avatar";
 
 const UserItem = (props) => {
   return (
     <li className="user-item">
-      <div className="user-item__constent">
-        <div className="user-item__image">
-          <img src={props.image} alt={props.name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{props.name}</h2>
-          <h3>
-            {props.placeCount} {props.placeCount === 1 ? "place" : "places"}
-          </h3>
-        </div>
+      <div className="user-item__content">
+        <Link to={`/${props.id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{props.name}</h2>
+            <h3>
+              {props.placeCount} {props.placeCount === 1 ? "place" : "places"}
+            </h3>
+          </div>
+        </Link>
       </div>
     </li>
   );
