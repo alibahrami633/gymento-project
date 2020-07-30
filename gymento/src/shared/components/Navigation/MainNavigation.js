70ccc7f241c93eea75f678f623e0eb2a2b93e7fb
Aliagho <ali.bahrami633@yahoo.com>
2020-07-30 16:32:55 +1000

    added navigation components

--- gymento/src/shared/components/Navigation/MainNavigation.js
+++ gymento/src/shared/components/Navigation/MainNavigation.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
+import MainHeader from "./MainHeader";
 import "./MainNavigation.css";
 
 const MainNavigation = () => {
-  return {};
+  return (
+    <MainHeader>
+      <botton className="main-navigation__menu-btn">
+        <span></span>
+        <span></span>
+        <span></span>
+      </botton>
+      <h1 className="main-navigation__title">
+        <Link to="/">Gymento</Link>
+      </h1>
+      <nav>...</nav>
+    </MainHeader>
+  );
 };
 
 export default MainNavigation;
