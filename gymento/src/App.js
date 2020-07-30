adccd3f09cd7e75a231a0105c104fbd642de1e43
Aliagho <ali.bahrami633@yahoo.com>
2020-07-30 12:36:08 +1000

    added routers and switch

--- gymento/src/App.js
+++ gymento/src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 import "./App.css";
 
 const App = () => {
-  return;
-  <Router></Router>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Users} />
+        <Route exact path="/places/new" component={NewPlace} />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
