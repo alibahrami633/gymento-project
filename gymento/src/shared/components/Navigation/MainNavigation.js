52a063ae86d2805a9a9cf1fce50aab309daf830b
Aliagho <ali.bahrami633@yahoo.com>
2020-07-30 20:55:41 +1000

    added sidebar

--- gymento/src/shared/components/Navigation/MainNavigation.js
+++ gymento/src/shared/components/Navigation/MainNavigation.js
@@ -3,23 +3,31 @@ import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
 import "./MainNavigation.css";
 
-const MainNavigation = () => {
+const MainNavigation = (props) => {
   return (
-    <MainHeader>
-      <botton className="main-navigation__menu-btn">
-        <span></span>
-        <span></span>
-        <span></span>
-      </botton>
-      <h1 className="main-navigation__title">
-        <Link to="/">Gymento</Link>
-      </h1>
-      <nav className="main-navigation__header-nav">
-        <NavLinks />
-      </nav>
-    </MainHeader>
+    <React.Fragment>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <botton className="main-navigation__menu-btn">
+          <span></span>
+          <span></span>
+          <span></span>
+        </botton>
+        <h1 className="main-navigation__title">
+          <Link to="/">Gymento</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   );
 };
 
--- gymento/src/shared/components/Navigation/SideDrawer.js
+++ gymento/src/shared/components/Navigation/SideDrawer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 import "./SideDrawer.css";
 
-const SideDrawer = () => {
-  return {};
+const SideDrawer = (props) => {
+  return <aside className="side-drawer">{props.children}</aside>;
 };
 
 export default SideDrawer;
