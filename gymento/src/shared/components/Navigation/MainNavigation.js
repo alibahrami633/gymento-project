2513102b62d88f63ee12a4ad599362bd4fda50a8
Aliagho <ali.bahrami633@yahoo.com>
2020-07-30 16:25:32 +1000

    added mainHeader

---
+++ gymento/src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./MainNavigation.css";
+
+const MainNavigation = () => {
+  return {};
+};
+
+export default MainNavigation;
--- gymento/src/user/components/UserItem.js
+++ gymento/src/user/components/UserItem.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import "./UserItem.css";
 import Avatar from "../../shared/components/UIElements/Avatar";
+import Card from "../../shared/components/UIElements/Card";
+import "./UserItem.css";
 
 const UserItem = (props) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
+      <Card className="user-item__content">
         <Link to={`/${props.id}/places`}>
           <div className="user-item__image">
             <Avatar image={props.image} alt={props.name} />
@@ -19,7 +20,7 @@ const UserItem = (props) => {
             </h3>
           </div>
         </Link>
-      </div>
+      </Card>
     </li>
   );
 };
