0071acdd92e1eec6b39f38bfc007374b8204429e
Aliagho <ali.bahrami633@yahoo.com>
2020-07-30 16:38:33 +1000

    added navigation to App

--- gymento/src/App.js
+++ gymento/src/App.js
@@ -6,19 +6,24 @@ import {
   Switch,
 } from "react-router-dom";
 
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 
 import "./App.css";
+import MainHeader from "./shared/components/Navigation/MainHeader";
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Users} />
-        <Route exact path="/places/new" component={NewPlace} />
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route exact path="/" component={Users} />
+          <Route exact path="/places/new" component={NewPlace} />
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 };
