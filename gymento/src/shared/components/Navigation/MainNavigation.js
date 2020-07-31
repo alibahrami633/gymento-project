7c011f198b2b56a938ad92f4e0dc4197dbfb15d3
Aliagho <ali.bahrami633@yahoo.com>
2020-07-31 13:13:23 +1000

    added useState to manage the sideDrawer state

--- gymento/src/shared/components/Navigation/MainNavigation.js
+++ gymento/src/shared/components/Navigation/MainNavigation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
@@ -7,13 +7,17 @@ import SideDrawer from "./SideDrawer";
 import "./MainNavigation.css";
 
 const MainNavigation = (props) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
   return (
     <React.Fragment>
-      <SideDrawer>
-        <nav className="main-navigation__drawer-nav">
-          <NavLinks />
-        </nav>
-      </SideDrawer>
+      {drawerIsOpen ? (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      ) : null}
       <MainHeader>
         <botton className="main-navigation__menu-btn">
           <span></span>
