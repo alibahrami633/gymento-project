21d333ea2334cc6e3e5b2a68a2ffb2791010230a
Aliagho <ali.bahrami633@yahoo.com>
2020-07-31 19:15:47 +1000

    added placeItem list component

--- gymento/src/places/components/PlaceItem.js
+++ gymento/src/places/components/PlaceItem.js
@@ -1,7 +1,29 @@
 import React from "react";
 
+import Card from "../../shared/components/UIElements/Card";
+import PlaceList from "./PlaceList";
 import "./PlaceItem.css";
 
-const PlaceItem = (props) => {};
+const PlaceItem = (props) => {
+  return (
+    <li className="place-item">
+      <Card className="class-item__content">
+        <div className="place-item__image">
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className="place-item__info">
+          <h2>{props.title}</h2>
+          <h3>{props.address}</h3>
+          <p>{props.description}</p>
+        </div>
+        <div className="place-item__actions">
+          <button>VIEW ON MAP</button>
+          <button>EDIT</button>
+          <button>DELETE</button>
+        </div>
+      </Card>
+    </li>
+  );
+};
 
 export default PlaceItem;
