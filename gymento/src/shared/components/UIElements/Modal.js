63c30331f5b7895a410a659bc23b7e4abeb2a68c
Aliagho <ali.bahrami633@yahoo.com>
2020-08-01 12:55:21 +1000

    added the Modal component and the required props to PlaceItems + added showMap state and handler to pop up the Modal on view on map button click

---
+++ gymento/src/shared/components/UIElements/Modal.js
@@ -0,0 +1,52 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
+
+import BackDrop from "./Backdrop";
+import "./Modal.css";
+
+/** the main modal content including header, content and footer */
+const ModalOverlay = (props) => {
+  const content = (
+    <div className={`modal ${props.className}`} style={props.style}>
+      <header className={`modal__header ${props.headerClass}`}>
+        <h2>{props.header}</h2>
+      </header>
+      <form
+        onSubmit={
+          props.onSubmit
+            ? props.onSubmit
+            : (event) => event.preventDefault.default()
+        }
+      >
+        <div className={`modal__content ${props.contentClass}`}>
+          {props.children}
+        </div>
+        <footer className={`modal__footer ${props.footerClass}`}>
+          {props.footer}
+        </footer>
+      </form>
+    </div>
+  );
+  return ReactDOM.createPortal(content, document.getElementById("modal-hook"));
+};
+
+/** to add background (BackDrop component) and animation to the mail modal overlay */
+const Modal = (props) => {
+  return (
+    <>
+      {props.show && <BackDrop onClick={props.onCancel} />}
+      <CSSTransition
+        in={props.show}
+        mountOnEnter
+        unmountOnExit
+        timeout={200}
+        classNames="modal"
+      >
+        <ModalOverlay {...props} />
+      </CSSTransition>
+    </>
+  );
+};
+
+export default Modal;
