68fc193523714b595126e06196c1baafaa6e9899
Aliagho <ali.bahrami633@yahoo.com>
2020-08-05 21:17:37 +1000

    added a context for athenticaiton and authorisation

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -12,24 +12,41 @@ import NewPlace from "./places/pages/NewPlace";
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import Auth from "./user/pages/Auth";
+import { AuthContext } from "./shared/Context/auth-context";
 
 import "./App.css";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = setState(false);
+
+  // to avoid re-creating it unnecessarily and to avoid infinite loops
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  });
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  });
+
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Switch>
-          <Route exact path="/" component={Users} />
-          <Route exact path="/:userId/places" component={UserPlaces} />
-          <Route exact path="/places/new" component={NewPlace} />
-          <Route exact path="/places/:placeId" component={UpdatePlace} />
-          <Route exact path="/auth" component={Auth} />
-          <Redirect to="/" />
-        </Switch>
-      </main>
-    </Router>
+    // value in AuthContext will binds the intial values in the AuthContext into a new value and by the value changing, all the components that listen to the context will be re-rendered
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedInl, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>
+          <Switch>
+            <Route exact path="/" component={Users} />
+            <Route exact path="/:userId/places" component={UserPlaces} />
+            <Route exact path="/places/new" component={NewPlace} />
+            <Route exact path="/places/:placeId" component={UpdatePlace} />
+            <Route exact path="/auth" component={Auth} />
+            <Redirect to="/" />
+          </Switch>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
