d73e0fb89c5ce9b7e98bacafcfeded295c974eab
Aliagho <ali.bahrami633@yahoo.com>
2020-08-06 22:18:15 +1000

    added deletePlace

--- back-end/controllers/places-controller.js
+++ back-end/controllers/places-controller.js
@@ -4,7 +4,7 @@ const { uuid } = require("uuidv4");
 
 const HttpError = require("../models/http-error");
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
   {
     id: "p1",
     title: "Empire Building",
@@ -93,7 +93,11 @@ const updatePlace = (req, res, next) => {
   res.status(200).json({ place: updatedPlace });
 };
 
-const deletePlace = (req, res, next) => {};
+const deletePlace = (req, res, next) => {
+  const placeId = req.params.pid;
+  DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
+  res.status(200).json({ message: "Place was deleted." });
+};
 
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
