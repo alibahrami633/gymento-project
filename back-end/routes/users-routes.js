d25e750688ace81df95face13490f02e37d8051e
Aliagho <ali.bahrami633@yahoo.com>
2020-08-07 19:17:01 +1000

    added user-routes

--- back-end/routes/users-routes.js
+++ back-end/routes/users-routes.js
@@ -1,3 +1,15 @@
 // middleware responsible for handling routes related to users
 
 const express = require("express");
+
+const usersControllers = require("../controllers/users-controller");
+
+const router = express.Router();
+
+router.get("/");
+
+router.post("/signup");
+
+router.post("/login");
+
+module.exports = router;
