b8bdae0f54a4a6592ad6de5a1b4eed54adb4ea5e
Aliagho <ali.bahrami633@yahoo.com>
2020-08-08 22:43:18 +1000

    added user model

---
+++ back-end/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: { type: String, requred: true },
+  email: { type: String, requred: true, unique: true },
+  password: { type: String, requred: true, minlength: 9 },
+  image: { type: String, requred: true },
+  places: { type: String, requred: true },
+});
+
+userSchema.plugin(uniqueValidator); // validates the email its unique property
+
+module.exports = mongoose.model("User", userSchema);
