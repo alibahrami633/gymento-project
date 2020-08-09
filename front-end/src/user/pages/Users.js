e334fa9ee88460a699a001768e5059cc78f9001b
Aliagho <ali.bahrami633@yahoo.com>
2020-08-09 13:40:50 +1000

    connected fetch users list from db to Users page

--- front-end/src/user/pages/Users.js
+++ front-end/src/user/pages/Users.js
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import UsersList from "../components/UsersList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 const Users = () => {
-  const USERS = [
-    {
-      id: "u1",
-      name: "Ali Bahrami",
-      image: "https://randomuser.me/api/portraits/lego/2.jpg",
-      places: 3,
-    },
-    {
-      id: "u2",
-      name: "John Doe",
-      image: "https://randomuser.me/api/portraits/lego/1.jpg",
-      places: 2,
-    },
-  ];
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
 
-  return <UsersList items={USERS} />;
+  // useEffect will stop the process falling into an infinite loop of rendering, it only renders once => ...,[]
+  useEffect(() => {
+    // never use asyn directly inside useEffect, instead use IIFE (Immediately Invoked Function Expression), it runs immediately after defining
+    const sendRequest = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://localhost:5000/api/users"); // fetch by default sends GET requests
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setLoadedUsers(responseData.users);
+      } catch (err) {
+        setIsLoading(false);
+        setError(err.message);
+      }
+      setIsLoading(false);
+    };
+
+    sendRequest();
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </>
+  );
 };
 
 export default Users;
