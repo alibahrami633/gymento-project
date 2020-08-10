87171c623d7881404aab3311852905598a1b74eb
Aliagho <ali.bahrami633@yahoo.com>
2020-08-10 19:19:37 +1000

    added a custom hook for auth and moved all hooks from App.js to the created hook

---
+++ front-end/src/shared/hooks/auth-hook.js
@@ -0,0 +1,62 @@
+import { useState, useCallback, useEffect } from "react";
+
+let logoutTimer;
+
+export const useAuth = () => {
+  const [token, setToken] = useState(false);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
+  const [userId, setUserId] = useState(false);
+
+  // to avoid re-creating it unnecessarily and to avoid infinite loops
+  const login = useCallback((uid, token, expirationDate) => {
+    setToken(token);
+    setUserId(uid);
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    setTokenExpirationDate(tokenExpirationDate);
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({
+        userId: uid,
+        token: token,
+        expiration: tokenExpirationDate.toISOString(), // toISOString is a special string which keeps the date format in a string and can be reversed to date late
+      })
+    );
+  }, []);
+
+  const logout = useCallback(() => {
+    setToken(null);
+    setTokenExpirationDate(null);
+    setUserId(null);
+    localStorage.removeItem("userData");
+  }, []);
+
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime =
+        tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime); // returns the id of the times
+    } else {
+      clearTimeout(logoutTimer); // clears the currently running timer based on its id
+    }
+  }, [token, logout, tokenExpirationDate]);
+
+  // for checking the local storage to see if a user is logged in. It only runs once at the start of the App
+  // login as a dependency here will not be re-rendered bacause we used useCallBack.
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("userData"));
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      );
+    }
+  }, [login]);
+
+  return { token, login, logout, userId };
+};
