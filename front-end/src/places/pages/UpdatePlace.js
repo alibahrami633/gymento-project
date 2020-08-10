8a2ebdf6a4090bd1c455e4a3634af24e4f123f34
Aliagho <ali.bahrami633@yahoo.com>
2020-08-10 17:01:19 +1000

    added token check to edit and delete place methods so only authorized user can do it

--- front-end/src/places/pages/UpdatePlace.js
+++ front-end/src/places/pages/UpdatePlace.js
@@ -75,6 +75,7 @@ const UpdatePlace = () => {
         }),
         {
           "Content-Type": "application/json",
+          Authorization: "Bearer " + auth.token,
         }
       );
       history.push(`/${auth.userId}/places`);
