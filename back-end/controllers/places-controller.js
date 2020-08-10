45dbf79adb759691b9747835a65f184b61612b6f
Aliagho <ali.bahrami633@yahoo.com>
2020-08-10 19:34:05 +1000

    code refactor

--- back-end/controllers/places-controller.js
+++ back-end/controllers/places-controller.js
@@ -87,7 +87,7 @@ const createPlace = async (req, res, next) => {
     return next(new HttpError("Invalid inputs, please check your data.", 422));
   }
 
-  const { title, description, address, creator } = req.body;
+  const { title, description, address } = req.body;
 
   let coordinates;
   try {
@@ -102,12 +102,12 @@ const createPlace = async (req, res, next) => {
     address,
     location: coordinates,
     image: req.file.path,
-    creator,
+    creator: req.userData.userId,
   });
 
   let user;
   try {
-    user = await User.findById(creator);
+    user = await User.findById(req.userData.userId);
   } catch (err) {
     return next(new HttpError("Could not find user by this id.", 404));
   }
