4a606823ba5a4317dba7129401b2e7d7f655c5f3
Aliagho <ali.bahrami633@yahoo.com>
2020-08-10 19:08:28 +1000

    added timer for autologin and auto logout

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,18 +6,21 @@ import {
   Switch,
 } from "react-router-dom";
 
-import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import Auth from "./user/pages/Auth";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import { AuthContext } from "./shared/Context/auth-context";
 
 import "./App.css";
 
+let logoutTimer;
+
 const App = () => {
   const [token, setToken] = useState(false);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
   const [userId, setUserId] = useState(false);
 
   // to avoid re-creating it unnecessarily and to avoid infinite loops
@@ -26,6 +29,7 @@ const App = () => {
     setUserId(uid);
     const tokenExpirationDate =
       expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    setTokenExpirationDate(tokenExpirationDate);
     localStorage.setItem(
       "userData",
       JSON.stringify({
@@ -38,9 +42,20 @@ const App = () => {
 
   const logout = useCallback(() => {
     setToken(null);
+    setTokenExpirationDate(null);
     setUserId(null);
     localStorage.removeItem("userData");
   }, []);
+
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime =
+        tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime); // returns the id of the times
+    } else {
+      clearTimeout(logoutTimer); // clears the currently running timer based on its id
+    }
+  }, [token, logout, tokenExpirationDate]);
 
   // for checking the local storage to see if a user is logged in. It only runs once at the start of the App
   // login as a dependency here will not be re-rendered bacause we used useCallBack.
@@ -51,7 +66,11 @@ const App = () => {
       storedData.token &&
       new Date(storedData.expiration) > new Date()
     ) {
-      login(storedData.id, storedData.token, Date(storedData.expiration));
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      );
     }
   }, [login]);
 
